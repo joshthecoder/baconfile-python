ef4b3826e0955e5d13fa727aca4a7b3b4daae932
adamzap <adamzap@gmail.com>
2009-07-14 16:08:10 -0700

    Fixed a few naming problems
    Made folder_items consistent across script
    Renamed file var to fetched_file

--- hello.py
+++ hello.py
@@ -4,12 +4,12 @@ import baconfile
 folder_items = baconfile.fetch_folder('someuser', 'somefolder')
 
 # Iterate over the folder and print the name and size
-for i in items:
+for i in folder_items:
   print 'name: %s  size: %i' % (i.name, i.size)
 
 # Let's download a file
-file = baconfile.fetch_file('someuser', 'somefolder/somefile.txt')
-raw_data = file.read_file()
+fetched_file = baconfile.fetch_file('someuser', 'somefolder/somefile.txt')
+raw_data = fetched_file.read_file()
 
 # Let's save the same file to our local system
-file.save_file('somefile.txt')
+fetched_file.save_file('somefile.txt')
